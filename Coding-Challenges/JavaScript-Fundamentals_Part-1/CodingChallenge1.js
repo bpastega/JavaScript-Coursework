f9587e4ccae6788cae65644023c00c58fb4aa166
bpastega <www.bianca.pastega@gmail.com>
2024-07-08 15:59:36 -0300

    Exercise solved

--- Coding-Challenges/JavaScript-Fundamentals_Part-1/CodingChallenge1.js
+++ Coding-Challenges/JavaScript-Fundamentals_Part-1/CodingChallenge1.js
@@ -11,3 +11,38 @@ Your tasks:
 Test data: 
   § Data 1: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95m tall. 
   § Data 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76m tall.*/
+
+/*
+Data 1:
+let markWeight = 78;
+let markHeight = 1.69;
+
+let johnWeight = 92;
+let johnHeight = 1.95;*/
+
+//Data 2
+let markWeight = 95;
+let markHeight = 1.88;
+
+let johnWeight = 85;
+let johnHeight = 1.76;
+
+let markHigherBMI;
+
+let markBMI = calculateBMI(markWeight, markHeight);
+let johnBMI = calculateBMI(johnWeight, johnHeight);
+
+if(markBMI>johnBMI){
+  markHigherBMI = true;
+  console.log("Mark's BMI is higher than John's BMI");
+}
+
+else{
+  markHigherBMI = false;
+  console.log("Mark's BMI is NOT higher than John's BMI");
+}
+
+function calculateBMI(mass, height){
+  let bmi = (mass/(height*height));
+  return bmi;
+}
